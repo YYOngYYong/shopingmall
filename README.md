5a11378f6e2f5f24f04cd1f02124d1a6dbdde746
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-08-09 22:19:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# shoppingmall
+ simple shoppingmall
---
+++ src/main.js
@@ -0,0 +1,12 @@
+//Fetch the items from the JSON file
+function loadItems() {
+  return fetch("./data/data.json")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+//main
+loadItems()
+  //promise가 return이 되면~
+  .then((items) => {})
+  .catch(console.log);
