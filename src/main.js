833695d73a48bec49451d3a2c11e1db130e469ef
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-08-10 15:01:47 +0900

    JS : display items

--- src/main.js
+++ src/main.js
@@ -2,11 +2,30 @@
 function loadItems() {
   return fetch("./data/data.json")
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => json.items);
+}
+
+// 동적으로 아이템리스트 출력
+function displayItems(items) {
+  const contaniner = document.querySelector(".items");
+  contaniner.innerHTML = items.map((item) => createHTMLString(item));
+}
+
+// 아이템 목록 보여주는 li 리스트
+function createHTMLString(item) {
+  return `
+      <li class="item">
+          <img src=${item.image} alt="${item.type}" class="item__thumbnail" />
+          <span class="item__description">${item.gender}, ${item.size}</span>
+      </li>
+  `;
 }
 
 //main
 loadItems()
   //promise가 return이 되면
-  .then((items) => {})
+  .then((items) => {
+    displayItems(items);
+    setEventListeners(items);
+  })
   .catch(console.log);
